7a0ea253a6c93317e650b1a81239ed28e74918ab
Nathan Mayall <70609396+nathanmayall@users.noreply.github.com>
2021-04-03 14:03:42 +0100

    add particles and center title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+
 import Icons from "./components/Icons";
+import Particles from "react-tsparticles";
+import particleOptions from "./components/particleOptions.json";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-gray-700 bg-gray-200">
-      <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider text-shadow-lg">
-          Nathan Mayall
-        </h1>
+    <Fragment>
+      <div className="absolute w-full h-full">
+        <Particles options={particleOptions} />
       </div>
-      <p className="my-6 tracking-wide">
-        <code>Junior Software Engineer</code>
-      </p>
-      <Icons />
-    </div>
+      <div className="relative flex flex-col items-center justify-center h-full text-gray-700">
+        <div className="flex items-center">
+          <h1 className="text-6xl font-thin tracking-wider text-shadow-lg">
+            Nathan Mayall
+          </h1>
+        </div>
+        <p className="my-6 tracking-wide">
+          <code>id='JuniorSoftwareEngineer'</code>
+        </p>
+        <Icons />
+      </div>
+    </Fragment>
   );
 }
 
