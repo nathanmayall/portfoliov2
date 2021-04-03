20e8c40d4264902b46a03e08c8d48debdf653688
Nathan Mayall <70609396+nathanmayall@users.noreply.github.com>
2021-04-03 10:53:36 +0100

    update title and add react-icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
+import { DiReact } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-full text-gray-700 bg-gray-200">
       <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider">
-          Create React App + Tailwind CSS
-        </h1>
+        <h1 className="text-6xl font-thin tracking-wider">Nathan Mayall</h1>
       </div>
       <p className="my-6 tracking-wide">
-        Edit <code>src/App.js</code> and save to reload.
+        <code>Junior Software Engineer</code>
       </p>
       <div className="flex justify-center mt-6">
         <a
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <DiReact className="text-6xl" />
         </a>
         <a
           className="ml-10 uppercase hover:underline"
@@ -26,7 +26,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Tailwind
+          <SiTailwindcss className="text-6xl" />
         </a>
       </div>
     </div>
